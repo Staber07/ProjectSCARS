15c8d7b62543aebd130b4c786051726ad413256a
Chris1320 <chris1320is@protonmail.com>
2025-02-02 22:49:07 +0800

    feat(frontend,wip): implementing login page
    Signed-off-by: Chris1320 <chris1320is@protonmail.com>

--- intransit/src/app/page.tsx
+++ intransit/src/app/page.tsx
@@ -1,101 +1,113 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { toast } from "sonner";
+import { Input } from "@/components/ui/input";
+import { Eye, EyeClosed } from "lucide-react";
+
+import { Program } from "@/lib/info";
+
+const formSchema = z.object({
+    username: z.string().min(1),
+    password: z.string().min(1),
+});
+
+export function ProfileForm() {
+    // 1. Define your form.
+    // 2. Define a submit handler.
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    const [passwordHiddenState, setPasswordHiddenState] = useState(true);
+    function submitUserLogin(values: z.infer<typeof formSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values);
+        toast("User `" + values.username + "` is logging in with password `" + values.password + "`");
+    }
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    function togglePasswordVisibility(e) {
+        e.preventDefault();
+        // console.log("Toggling password visibility");
+        const passwordInput = document.getElementById("form-login-password") as HTMLInputElement;
+        console.log(passwordInput);
+        // FIXME: If setPasswordHiddenState is called here, passwordInput.type is not updated.
+        passwordInput.type = passwordInput.type === "password" ? "text" : "password";
+        // setPasswordHiddenState(!passwordHiddenState);
+    }
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            username: "",
+            password: "",
+        },
+    });
+
+    return (
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+                <h1 className="text-4xl font-bold text-center sm:text-left">
+                    Welcome to <span>{Program.name}</span>
+                </h1>
+                <h2 className="text-2xl text-center sm:text-left">{Program.description}</h2>
+                <div className="w-full max-w-sm">
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(submitUserLogin)} className="space-y-5">
+                            <FormField
+                                control={form.control}
+                                name="username"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        {/* <FormLabel>Username</FormLabel> */}
+                                        <FormControl>
+                                            <Input
+                                                id="form-login-username"
+                                                type="text"
+                                                placeholder="Username"
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                        {/* <FormDescription></FormDescription> */}
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            {/* TODO: Adjust alignment */}
+                            <div className="flex justify-between">
+                                <FormField
+                                    control={form.control}
+                                    name="password"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            {/* <FormLabel>Password</FormLabel> */}
+                                            <FormControl>
+                                                <Input
+                                                    id="form-login-password"
+                                                    type="password"
+                                                    placeholder="Password"
+                                                    {...field}
+                                                />
+                                            </FormControl>
+                                            {/* <FormDescription></FormDescription> */}
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                                <Button onClick={togglePasswordVisibility}>
+                                    {passwordHiddenState ? <Eye /> : <EyeClosed />}
+                                </Button>
+                            </div>
+                            <Button type="submit">Submit</Button>
+                        </form>
+                    </Form>
+                </div>
+            </main>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
 }
