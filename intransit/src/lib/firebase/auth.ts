f6c32fa5e88fa95eca37b97a7ed549742c1063cc
Chris1320 <chris1320is@protonmail.com>
2025-02-11 04:41:40 +0800

    feat(frontend): Add Firebase initialization and function contact utility; remove obsolete auth module
    Signed-off-by: Chris1320 <chris1320is@protonmail.com>

--- intransit/src/lib/firebase/auth.ts
+++
@@ -1,4 +0,0 @@
-import { getAuth } from "firebase/auth";
-import { firebaseApp } from "@/lib/firebase/firebase";
-
-export const firebaseAuth = getAuth(firebaseApp);
--- intransit/src/lib/firebase/firebase.ts
+++
@@ -1,6 +0,0 @@
-import { initializeApp } from "firebase/app";
-
-import { firebaseConfig } from "@/lib/firebase/_config";
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-export const firebaseApp = initializeApp(firebaseConfig);
