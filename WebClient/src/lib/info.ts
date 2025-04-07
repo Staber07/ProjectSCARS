a6921d77d8d9d60bbf3906adbc71de374c94a5c2
Chris1320 <me@chrisfromnowhere.me>
2025-04-07 17:52:00 +0800

    feat(web-server): add toast notification handler
    Signed-off-by: Chris1320 <me@chrisfromnowhere.me>

--- WebClient/src/lib/info.ts
+++ WebClient/src/lib/info.ts
@@ -1,5 +1,21 @@
 export const Program = {
   name: "Project SCARS",
   description: "School Canteen Automated Reporting System",
-  version: "0.1.0",
+  version: "0.3.0",
+};
+
+export const Connections = {
+  CentralServer: {
+    host: "localhost",
+    port: 8000,
+  },
+  LocalServer: {
+    // TODO: WIP
+    host: "localhost",
+    port: 8001,
+  },
+};
+
+export const LocalStorage = {
+  jwt_name: "t",
 };
