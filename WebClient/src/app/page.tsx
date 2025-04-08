3821bd0ffc73a60a979937031f9cfbda8eabb7c1
Chris1320 <me@chrisfromnowhere.me>
2025-04-08 08:26:55 +0800

    feat(web-client): redirect from `/` to `/login`
    Signed-off-by: Chris1320 <me@chrisfromnowhere.me>

--- WebClient/src/app/page.tsx
+++ WebClient/src/app/page.tsx
@@ -1,4 +1,33 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { useAuth, AuthProvider } from "@/lib/auth";
+
+/// Wrapper for the entire page
+// to enable the use of the AuthProvider
 export default function Home() {
+  return (
+    <AuthProvider>
+      <HomeContent />
+    </AuthProvider>
+  );
+}
+
+function HomeContent() {
+  const { is_authenticated } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!is_authenticated) {
+      router.push("/login"); // Redirect to the login page if not authenticated
+    }
+  }, [is_authenticated, router]);
+
+  if (!is_authenticated) {
+    return null; // TODO: WIP
+  }
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
