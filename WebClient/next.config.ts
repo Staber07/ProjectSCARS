4fd920294501971437a3f1728bad6e77d25984df
Chris1320 <me@chrisfromnowhere.me>
2025-04-19 21:05:55 +0800

    chore(web-client): remove current project to use mantine project init script
    Signed-off-by: Chris1320 <me@chrisfromnowhere.me>

--- WebClient/next.config.ts
+++
@@ -1,7 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  // devIndicators: false, // disable the default Next.js dev indicators
-};
-
-export default nextConfig;
