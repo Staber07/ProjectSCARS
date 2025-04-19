ca47c387749ee5c2cdcc5415d2c4f0862c3cb454
Chris1320 <me@chrisfromnowhere.me>
2025-04-19 23:06:49 +0800

    test(web-client): setup testing and code coverage in web client
    Signed-off-by: Chris1320 <me@chrisfromnowhere.me>

---
+++ WebClient/__tests__/example.test.tsx
@@ -0,0 +1,6 @@
+import { expect, test } from "vitest";
+import { Program } from "@/lib/info";
+
+test("Example Test", () => {
+  expect(Program.name).toBe("Project SCARS");
+});
--- WebClient/vite.config.js
+++ WebClient/vite.config.js
@@ -1,6 +1,8 @@
 // vite.config.js
 import { defineConfig } from "vite";
 import { codecovVitePlugin } from "@codecov/vite-plugin";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   test: {
@@ -9,6 +11,8 @@ export default defineConfig({
     setupFiles: "./vitest.setup.mjs",
   },
   plugins: [
+    tsconfigPaths(),
+    react(),
     // Put the Codecov vite plugin after all other plugins
     codecovVitePlugin({
       enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
