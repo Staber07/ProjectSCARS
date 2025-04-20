f7f15784b291704402f5b8eb217ab934fd946258
Chris1320 <me@chrisfromnowhere.me>
2025-04-20 23:23:33 +0800

    fix(web-client): temporarily remove `postcss-simple-vars` config
    Signed-off-by: Chris1320 <me@chrisfromnowhere.me>

--- WebClient/postcss.config.mjs
+++ WebClient/postcss.config.mjs
@@ -6,17 +6,18 @@ const config = {
   ],
 };
 
-// Adding postcss-simple-vars configuration
-config.plugins.push({
-  "postcss-simple-vars": {
-    variables: {
-      "mantine-breakpoint-xs": "36em",
-      "mantine-breakpoint-sm": "48em",
-      "mantine-breakpoint-md": "62em",
-      "mantine-breakpoint-lg": "75em",
-      "mantine-breakpoint-xl": "88em",
-    },
-  },
-});
+// FIXME:
+// // Adding postcss-simple-vars configuration
+// config.plugins.push({
+//   "postcss-simple-vars": {
+//     variables: {
+//       "mantine-breakpoint-xs": "36em",
+//       "mantine-breakpoint-sm": "48em",
+//       "mantine-breakpoint-md": "62em",
+//       "mantine-breakpoint-lg": "75em",
+//       "mantine-breakpoint-xl": "88em",
+//     },
+//   },
+// });
 
 export default config;
