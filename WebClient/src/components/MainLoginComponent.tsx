210d077f740ca213eef1ee8754175ab7ad8d1c06
Chris1320 <me@chrisfromnowhere.me>
2025-04-21 21:36:59 +0800

    feat(web-client): functional login page
    Signed-off-by: Chris1320 <me@chrisfromnowhere.me>

--- WebClient/src/components/MainLoginComponent.tsx
+++ WebClient/src/components/MainLoginComponent.tsx
@@ -1,7 +1,8 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import {
-  Anchor,
+  // Anchor,
   Button,
   Checkbox,
   Container,
@@ -12,10 +13,47 @@ import {
   TextInput,
   Title,
 } from "@mantine/core";
+import { useForm } from "@mantine/form";
+
+import ky from "ky";
+
 import classes from "@/components/MainLoginComponent.module.css";
-import { Program } from "@/lib/info";
+import { Connections, Program } from "@/lib/info";
+import { useAuth } from "@/lib/providers/auth";
+import { AccessTokenType } from "@/lib/types";
 
 export function MainLoginComponent() {
+  const router = useRouter();
+  const auth = useAuth();
+  const form = useForm({
+    mode: "uncontrolled",
+    initialValues: { username: "", password: "", rememberMe: false },
+  });
+
+  /// Login function
+  const loginUser = async (values: {
+    username: string;
+    password: string;
+    rememberMe: boolean;
+  }) => {
+    try {
+      const loginFormData = new URLSearchParams();
+      loginFormData.set("grant_type", "password");
+      loginFormData.set("username", values.username);
+      loginFormData.set("password", values.password);
+
+      const centralServerResponse = await ky.post(
+        `${Connections.CentralServer.endpoint}/api/v1/auth/token`,
+        { body: loginFormData },
+      );
+      const responseData: AccessTokenType = await centralServerResponse.json();
+      auth.login(responseData["access_token"]);
+      router.push("/");
+    } catch (error) {
+      console.error("Error logging in:", error);
+    }
+  };
+
   return (
     <Container size={420} my={40}>
       <Title ta="center" className={classes.title}>
@@ -25,22 +63,35 @@ export function MainLoginComponent() {
         {Program.description}
       </Text>
       <Paper withBorder shadow="md" p={30} mt={30} radius="md">
-        <TextInput label="Username" placeholder="Your username" required />
-        <PasswordInput
-          label="Password"
-          placeholder="Your password"
-          required
-          mt="md"
-        />
-        <Group justify="space-between" mt="lg">
-          <Checkbox label="Remember me" />
-          <Anchor component="button" size="sm">
-            Forgot password?
-          </Anchor>
-        </Group>
-        <Button fullWidth mt="xl">
-          Sign in
-        </Button>
+        <form onSubmit={form.onSubmit(loginUser)}>
+          <TextInput
+            label="Username"
+            placeholder="Your username"
+            key={form.key("username")}
+            {...form.getInputProps("username")}
+            required
+          />
+          <PasswordInput
+            label="Password"
+            placeholder="Your password"
+            key={form.key("password")}
+            {...form.getInputProps("password")}
+            required
+            mt="md"
+          />
+          <Group justify="space-between" mt="lg">
+            <Checkbox
+              label="Remember me"
+              {...form.getInputProps("rememberMe", { type: "checkbox" })}
+            />
+            {/* <Anchor component="button" size="sm"> */}
+            {/*   Forgot password? */}
+            {/* </Anchor> */}
+          </Group>
+          <Button type="submit" fullWidth mt="xl">
+            Sign in
+          </Button>
+        </form>
       </Paper>
     </Container>
   );
---
+++ WebClient/src/lib/types.ts
@@ -0,0 +1,4 @@
+export type AccessTokenType = {
+  access_token: string;
+  token_type: string;
+};
