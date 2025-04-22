0c2100321ae27e246b771467dfaee983238ac75a
Chris1320 <me@chrisfromnowhere.me>
2025-04-22 22:31:22 +0800

    fix(web-client): try to please CI, Vitest, and NextJS all at once
    Signed-off-by: Chris1320 <me@chrisfromnowhere.me>

--- WebClient/postcss.config.mjs
+++ WebClient/postcss.config.mjs
@@ -1,20 +1,19 @@
-import tailwindcss from "@tailwindcss/postcss";
-import presetMantine from "postcss-preset-mantine";
-import simpleVars from "postcss-simple-vars";
-
 const config = {
   plugins: [
-    tailwindcss,
-    presetMantine,
-    simpleVars({
-      variables: {
-        "mantine-breakpoint-xs": "36em",
-        "mantine-breakpoint-sm": "48em",
-        "mantine-breakpoint-md": "62em",
-        "mantine-breakpoint-lg": "75em",
-        "mantine-breakpoint-xl": "88em",
+    "@tailwindcss/postcss",
+    "postcss-preset-mantine",
+    [
+      "postcss-simple-vars",
+      {
+        variables: {
+          "mantine-breakpoint-xs": "36em",
+          "mantine-breakpoint-sm": "48em",
+          "mantine-breakpoint-md": "62em",
+          "mantine-breakpoint-lg": "75em",
+          "mantine-breakpoint-xl": "88em",
+        },
       },
-    }),
+    ],
   ],
 };
 
