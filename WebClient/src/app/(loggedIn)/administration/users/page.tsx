a7aaffc3006a67d44c821ae6029fd5fd31c985cb
Kirito090504 <tenchieadolfo05@gmail.com>
2025-04-22 23:10:56 +0800

    feat: enhance user layout and search functionality in administration page

--- WebClient/src/app/(loggedIn)/administration/users/page.tsx
+++ WebClient/src/app/(loggedIn)/administration/users/page.tsx
@@ -1,7 +1,16 @@
+"use client";
+
 import { AppShell, Burger, Group, Skeleton } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 
-export function rootContent() {
+import { TextInput } from '@mantine/core';
+import { ActionIcon } from '@mantine/core';
+//import { IconSearch } from '@tabler/icons-react';
+
+import { Flex } from '@mantine/core';
+
+
+export default function rootContent() {
   const [opened, { toggle }] = useDisclosure();
 
   return (
@@ -11,19 +20,44 @@ export function rootContent() {
       padding="md"
     >
       <AppShell.Header>
+        header
         <Group h="100%" px="md">
           <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
         </Group>
       </AppShell.Header>
       <AppShell.Navbar p="md">
-        Navbar
+        guard
         {Array(15)
           .fill(0)
           .map((_, index) => (
             <Skeleton key={index} h={28} mt="sm" animate={false} />
           ))}
       </AppShell.Navbar>
-      <AppShell.Main>Main</AppShell.Main>
+      <AppShell.Main>
+        <Flex
+          mih={50}
+          //bg="rgba(255, 255, 255, 0.3)"
+          gap="md"
+          justify="flex-start"
+          align="flex-start"
+          direction="row"
+          wrap="wrap"
+        >
+          <TextInput
+            variant="filled"
+            label="Search"
+            placeholder=""
+          />
+
+          <ActionIcon
+            variant="gradient"
+            size="xl"
+            aria-label="Gradient action icon"
+            gradient={{ from: 'blue', to: 'cyan', deg: 90 }}
+          >
+          </ActionIcon>
+        </Flex>
+      </AppShell.Main>
     </AppShell>
   );
 }
