2a93a97edb2f6571334f26058b247c073dd955e7
Kirito090504 <tenchieadolfo05@gmail.com>
2025-05-04 21:46:38 +0800

    feat: refactor dashboard layout and add Navbar component

--- WebClient/src/app/(loggedIn)/Dashboard/page.tsx
+++ WebClient/src/app/(loggedIn)/Dashboard/page.tsx
@@ -1,14 +1,12 @@
 "use client";
 
-import { AppShell } from '@mantine/core';
+import { Flex } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 
 export default function rootContent() {
     const [opened, { toggle }] = useDisclosure();
 
     return (
-        <AppShell>
-            
-        </AppShell>
+        <Flex></Flex>
     );
 }
--- WebClient/src/app/(loggedIn)/layout.tsx
+++ WebClient/src/app/(loggedIn)/layout.tsx
@@ -1,38 +1,18 @@
 "use client";
 
-import { AppShell, Burger } from '@mantine/core';
-import { useDisclosure } from '@mantine/hooks';
+import { Navbar } from '@/components/Navbar';
+
 
 export default function rootLayout({
     children,
 }: {
     children: React.ReactNode
 }) {
-    const [opened, { toggle }] = useDisclosure();
+
+
 
     return (
-        <AppShell
-            header={{ height: 60 }}
-            navbar={{
-                width: 300,
-                breakpoint: 'sm',
-                collapsed: { mobile: !opened },
-            }}
-            padding="md"
-        >
-            <AppShell.Header>
-                <Burger
-                    opened={opened}
-                    onClick={toggle}
-                    hiddenFrom="sm"
-                    size="sm"
-                />
-                <div>Logo</div>
-            </AppShell.Header>
+        <Navbar />
 
-            <AppShell.Navbar p="md">Navbar</AppShell.Navbar>
-
-            <AppShell.Main>{children}</AppShell.Main>
-        </AppShell>
     );
 }
