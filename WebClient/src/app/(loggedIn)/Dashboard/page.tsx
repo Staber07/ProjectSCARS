391353610683c6468b44caeea0cb9cb9d8d0d6f3
Kirito090504 <tenchieadolfo05@gmail.com>
2025-05-04 21:25:17 +0800

    feat: add new pages for user profile, reports, statistics, and administration sections

--- WebClient/src/app/(loggedIn)/Dashboard/page.tsx
+++ WebClient/src/app/(loggedIn)/Dashboard/page.tsx
@@ -1,34 +1,14 @@
 "use client";
 
-import { AppShell, Burger, Skeleton, Center, Group, keys, ScrollArea, Text, TextInput, UnstyledButton, AppShellNavbar, AppShellMain } from '@mantine/core';
-//import { useState } from 'react';
+import { AppShell } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
-//import { Table, Avatar } from '@mantine/core';
-//import { IconChevronDown, IconChevronUp, IconSearch, IconSelector } from '@tabler/icons-react';
-
 
 export default function rootContent() {
     const [opened, { toggle }] = useDisclosure();
 
     return (
         <AppShell>
-            <AppShell.Header>
-
-            </AppShell.Header>
-
-            <AppShellNavbar>
-                <Burger
-                    opened={opened}
-                    onClick={toggle}
-                    hiddenFrom="sm"
-                    size="sm"
-                />
-            </AppShellNavbar>
-
-            <AppShellMain>
-
-
-            </AppShellMain>
+            
         </AppShell>
     );
 }
