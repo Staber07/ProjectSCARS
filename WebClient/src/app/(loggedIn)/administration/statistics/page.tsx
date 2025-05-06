e72ea24edc3236d9fb71321e6637aa00ecdc1530
Kirito090504 <tenchieadolfo05@gmail.com>
2025-05-06 21:55:04 +0800

    feat(statistics): update statistics page with line charts

--- WebClient/src/app/(loggedIn)/administration/statistics/page.tsx
+++ WebClient/src/app/(loggedIn)/administration/statistics/page.tsx
@@ -1,8 +1,48 @@
 "use client";
 
-import { Text } from "@mantine/core";
+import { Text, Flex } from "@mantine/core";
+import { LineChart } from "@mantine/charts";
+
+import { data } from './data';
 
 export default function StatisticsPage() {
   console.debug("Rendering StatisticsPage");
-  return <Text>Statistics</Text>;
+  return (
+    <div>
+      <Flex
+        mih={50}
+        gap="xl"
+        justify="center"
+        align="flex-start"
+        direction="row"
+        wrap="wrap"
+      >
+        <Text mb="md" pl="md">
+          Apples sales:
+        </Text>
+
+        <LineChart
+          h={180}
+          data={data}
+          dataKey="date"
+          series={[{ name: 'Apples', color: 'indigo.6' }]}
+          lineChartProps={{ syncId: 'groceries' }}
+        />
+
+        <Text mb="md" pl="md" mt="xl">
+          Tomatoes sales:
+        </Text>
+
+        <LineChart
+          h={180}
+          data={data}
+          dataKey="date"
+          lineChartProps={{ syncId: 'groceries' }}
+          series={[{ name: 'Tomatoes', color: 'teal.6' }]}
+        />
+
+
+      </Flex>
+    </div>
+  );
 }
