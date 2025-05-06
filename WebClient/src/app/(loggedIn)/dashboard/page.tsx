34d6af044f34121c1e32797bd848f086c6cc744f
Chris1320 <me@chrisfromnowhere.me>
2025-05-06 09:57:12 +0800

    fix(web-client): fix non-stop re-render of dashboard
    Signed-off-by: Chris1320 <me@chrisfromnowhere.me>

--- WebClient/src/app/(loggedIn)/dashboard/page.tsx
+++ WebClient/src/app/(loggedIn)/dashboard/page.tsx
@@ -4,17 +4,31 @@ import { useEffect, useState } from "react";
 import { Text } from "@mantine/core";
 import { CentralServerGetUserInfo } from "@/lib/api/auth";
 import { UserPublicType } from "@/lib/types";
+import { LoadingComponent } from "@/components/LoadingComponent";
 
 export default function DashboardPage() {
   const [userInfo, setUserInfo] = useState<UserPublicType | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const fetchUserInfo = async () => {
+    console.debug("Fetching user info");
+    setUserInfo(await CentralServerGetUserInfo());
+    setIsLoading(false);
+  };
+
   useEffect(() => {
     console.debug("DashboardPage useEffect started");
-    const fetchUserInfo = async () => {
-      setUserInfo(await CentralServerGetUserInfo());
-    };
     fetchUserInfo();
-  });
+  }, []);
+
   console.debug("Rendering DashboardPage");
   // TODO: WIP
-  return <Text>{JSON.stringify(userInfo)}</Text>;
+  if (isLoading) {
+    return <LoadingComponent message="Loading data..." withBorder={false} />;
+  }
+
+  return (
+    <Text>
+      {userInfo ? JSON.stringify(userInfo) : "No user info available"}
+    </Text>
+  );
 }
