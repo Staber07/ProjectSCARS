e0c682a141b9d74fc772cf0aeceb90f2f55d0fd9
Kirito090504 <tenchieadolfo05@gmail.com>
2025-05-06 22:12:10 +0800

    feat(statistics): add responsive container styles and refactor statistics page layout

--- WebClient/src/app/(loggedIn)/administration/statistics/page.tsx
+++ WebClient/src/app/(loggedIn)/administration/statistics/page.tsx
@@ -1,48 +1,38 @@
 "use client";
 
-import { Text, Flex } from "@mantine/core";
+import { Text, MantineProvider, Container, createTheme, Flex } from "@mantine/core";
 import { LineChart } from "@mantine/charts";
 
 import { data } from './data';
+import cx from 'clsx';
+import classes from './container.module.css';
+
+const theme = createTheme({
+  components: {
+    Container: Container.extend({
+      classNames: (_, { size }) => ({
+        root: cx({ [classes.responsiveContainer]: size === 'responsive' }),
+      }),
+    }),
+  },
+});
 
 export default function StatisticsPage() {
   console.debug("Rendering StatisticsPage");
   return (
-    <div>
-      <Flex
-        mih={50}
-        gap="xl"
-        justify="center"
-        align="flex-start"
-        direction="row"
-        wrap="wrap"
-      >
-        <Text mb="md" pl="md">
-          Apples sales:
-        </Text>
 
-        <LineChart
-          h={180}
-          data={data}
-          dataKey="date"
-          series={[{ name: 'Apples', color: 'indigo.6' }]}
-          lineChartProps={{ syncId: 'groceries' }}
-        />
-
-        <Text mb="md" pl="md" mt="xl">
-          Tomatoes sales:
-        </Text>
-
-        <LineChart
-          h={180}
-          data={data}
-          dataKey="date"
-          lineChartProps={{ syncId: 'groceries' }}
-          series={[{ name: 'Tomatoes', color: 'teal.6' }]}
-        />
-
-
-      </Flex>
-    </div>
+      <MantineProvider theme={theme}>
+        <Container size="responsive" bg="var(--mantine-color-blue-light)">
+          <LineChart
+            h={500}
+            data={data}
+            dataKey="date"
+            withLegend
+            series={[
+              { name: 'Apples', color: 'indigo.6' },
+            ]}
+          />
+        </Container>
+      </MantineProvider>
   );
 }
