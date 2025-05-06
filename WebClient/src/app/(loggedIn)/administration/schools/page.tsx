3782810af0015330143f3128c725b406ebadfb61
Kirito090504 <tenchieadolfo05@gmail.com>
2025-05-06 22:47:43 +0800

    feat(schools): implement schools page with search and pagination features

--- WebClient/src/app/(loggedIn)/administration/schools/page.tsx
+++ WebClient/src/app/(loggedIn)/administration/schools/page.tsx
@@ -1,8 +1,69 @@
 "use client";
 
-import { Text } from "@mantine/core";
+import { Text, Table, Flex, ActionIcon, TextInput, Pagination, Group } from "@mantine/core";
+import { IconSearch } from "@tabler/icons-react";
+
+
+const tableData = {
+  //caption: 'Different Schools in Baliuag Division',
+  head: ['Schools', 'Address', 'Net Income', 'Net Profit', 'Gross Profit'],
+  body: [
+    ['Hinukay', 'Baliuag, Bulacan', 3000, 100, 2000],
+    ['Pinagbarilan', 'Baliuag, Bulacan', 5000, 400, 1000],
+    ['Calantipay', 'Baliuag, Bulacan', 6000, 500, 1000],
+    ['Tilapayong', 'Baliuag, Bulacan', 6000, 400, 1000],
+    ['Poblacion', 'Baliuag, Bulacan', 100, 100, 2000],
+  ],
+};
 
 export default function SchoolsPage() {
   console.debug("Rendering SchoolsPage");
-  return <Text>Schools</Text>;
+
+  return (
+    <div>
+      <Flex
+        mih={50}
+        gap="xl"
+        justify="flex-start"
+        align="flex-start"
+        direction="row"
+        wrap="nowrap"
+      >
+
+        <TextInput
+          placeholder="Search for users"
+          size="md"
+          style={{ width: "400px" }}
+        />
+        <ActionIcon
+          style={{ marginLeft: "300px" }}
+          size="input-md"
+          variant="default"
+          aria-label="ActionIcon the same size as inputs"
+        >
+          {<IconSearch size={16} />}
+        </ActionIcon>
+
+      </Flex>
+
+      <Table
+        stickyHeader
+        stickyHeaderOffset={60}
+        verticalSpacing={"sm"}
+        highlightOnHover
+        withTableBorder
+        data={tableData}
+      />;
+
+      <Pagination.Root total={10}>
+        <Group gap={10} justify="center">
+          <Pagination.First />
+          <Pagination.Previous />
+          <Pagination.Items />
+          <Pagination.Next />
+          <Pagination.Last />
+        </Group>
+      </Pagination.Root>
+    </div>
+  );
 }
