6d85be74ba96b01b137005e7c8f39026bbbfb27d
Kirito090504 <tenchieadolfo05@gmail.com>
2025-05-13 23:02:25 +0800

    feat(statistics): add sales data and update statistics page layout

--- WebClient/src/app/(loggedIn)/administration/statistics/page.tsx
+++ WebClient/src/app/(loggedIn)/administration/statistics/page.tsx
@@ -1,39 +1,98 @@
 "use client";
 
-import { MantineProvider, Container, createTheme } from "@mantine/core";
-import { LineChart } from "@mantine/charts";
+import {
+  IconArrowDownRight,
+  IconArrowUpRight,
+  IconCoin,
+  IconDiscount2,
+  IconReceipt2,
+  IconUserPlus,
+} from '@tabler/icons-react';
+import { Group, Paper, SimpleGrid, Text, Flex, Grid } from '@mantine/core';
+import { LineChart } from '@mantine/charts';
 
-import { data } from './data';
-import cx from 'clsx';
-import classes from './container.module.css';
 
-const theme = createTheme({
-  components: {
-    Container: Container.extend({
-      classNames: (_, { size }) => ({
-        root: cx({ [classes.responsiveContainer]: size === 'responsive' }),
-      }),
-    }),
-  },
-});
+import classes from './Stats.module.css';
+
+
+import { data } from './NetSalesdata';
+
+const icons = {
+  user: IconUserPlus,
+  discount: IconDiscount2,
+  receipt: IconReceipt2,
+  coin: IconCoin,
+};
+
+const TopStats = [
+  { title: 'Net Sales', icon: 'receipt', value: '13,456', diff: 34 },
+  { title: 'Gross Profit', icon: 'coin', value: '4,145', diff: -13 },
+  { title: 'Cost of Sales', icon: 'discount', value: '745', diff: 18 },
+  { title: 'Net Income', icon: 'user', value: '188', diff: -30 },
+] as const;
+
 
 export default function StatisticsPage() {
   console.debug("Rendering StatisticsPage");
+
+  const stats = TopStats.map((stat) => {
+    const Icon = icons[stat.icon];
+    const DiffIcon = stat.diff > 0 ? IconArrowUpRight : IconArrowDownRight;
+
+    return (
+      <Paper withBorder p="md" radius="md" key={stat.title}>
+        <Group justify="space-between">
+          <Text size="xs" c="dimmed" className={classes.title}>
+            {stat.title}
+          </Text>
+          <Icon className={classes.icon} size={22} stroke={1.5} />
+        </Group>
+
+        <Group align="flex-end" gap="xs" mt={25}>
+          <Text className={classes.value}>{stat.value}</Text>
+          <Text c={stat.diff > 0 ? 'teal' : 'red'} fz="sm" fw={500} className={classes.diff}>
+            <span>{stat.diff}%</span>
+            <DiffIcon size={16} stroke={1.5} />
+          </Text>
+        </Group>
+
+        <Text fz="xs" c="dimmed" mt={7}>
+          Compared to previous month
+        </Text>
+      </Paper>
+    );
+  });
+
   return (
-  
-      <MantineProvider theme={theme}>
-        <Container size="responsive" bg="var(--mantine-color-blue-light)">
-          <LineChart
-            h={500}
-            data={data}
-            dataKey="date"
-            withLegend
-            series={[
-              { name: 'Apples', color: 'indigo.6' },
-            ]}
-          />
-        </Container>
-      </MantineProvider>
-    
+    <div className={classes.root}>
+      <SimpleGrid cols={{ base: 1, xs: 2, md: 4 }}>{stats}</SimpleGrid>
+
+      <div style={{ marginTop: '10px' }}>
+
+        <div style={{ resize: 'horizontal', overflow: 'hidden', maxWidth: '100%' }}>
+          <Grid
+            type="container"
+            breakpoints={{ xs: '100px', sm: '200px', md: '300px', lg: '400px', xl: '500px' }}
+          >
+            <Grid.Col span={{ base: 12, md: 6, lg: 5 }}><LineChart
+              h={300}
+              data={data}
+              dataKey="date"
+              series={[{ name: 'sales', color: 'indigo.6' }]}
+              curveType="monotone"
+              connectNulls
+            /></Grid.Col>
+            <Grid.Col span={{ base: 12, md: 6, lg: 5 }}>2</Grid.Col>
+            <Grid.Col span={{ base: 12, md: 6, lg: 5 }}>3</Grid.Col>
+            <Grid.Col span={{ base: 12, md: 6, lg: 5 }}>4</Grid.Col>
+          </Grid>
+        </div>
+
+
+
+      </div>
+
+    </div>
+
   );
 }
