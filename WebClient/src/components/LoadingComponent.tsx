ee01fac0214357770f6deda1d8ff241778147dd9
Chris1320 <me@chrisfromnowhere.me>
2025-05-15 12:16:09 +0800

    fix(web-client): center loading component
    Signed-off-by: Chris1320 <me@chrisfromnowhere.me>

--- WebClient/src/components/LoadingComponent.tsx
+++ WebClient/src/components/LoadingComponent.tsx
@@ -1,4 +1,4 @@
-import { Center, Image, Loader, Paper, Stack, Text, Title } from "@mantine/core";
+import { Center, Container, Image, Loader, Paper, Stack, Text, Title } from "@mantine/core";
 
 import { Program } from "@/lib/info";
 
@@ -18,17 +18,19 @@ export const LoadingComponent: React.FC<LoadingComponentProps> = ({
 }) => {
     console.debug("Returning LoadingComponent", { message });
     return (
-        <Center>
-            <Paper withBorder={withBorder} shadow="md" p={30} mt={30} radius="md">
-                <Stack align="center" justify="center" gap="xs">
-                    <Image src="/assets/BENTOLogo.svg" alt="BENTO Logo" width={100} height={100} />
-                    <Title className={classes.title} data-testid="loading-title">
-                        {Program.name}
-                    </Title>
-                    {message && <Text data-testid="loading-message">{message}</Text>}
-                    <Loader color="blue" type="bars" />
-                </Stack>
-            </Paper>
-        </Center>
+        <Container size={420} my={40} style={{ paddingTop: "150px" }}>
+            <Center>
+                <Paper withBorder={withBorder} shadow="md" p={30} mt={30} radius="md">
+                    <Stack align="center" justify="center" gap="xs">
+                        <Image src="/assets/BENTOLogo.svg" alt="BENTO Logo" width={100} height={100} />
+                        <Title className={classes.title} data-testid="loading-title">
+                            {Program.name}
+                        </Title>
+                        {message && <Text data-testid="loading-message">{message}</Text>}
+                        <Loader color="blue" type="bars" />
+                    </Stack>
+                </Paper>
+            </Center>
+        </Container>
     );
 };
