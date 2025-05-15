48e0112ba21481a4c41d1bf66f042c2c075c8adb
Chris1320 <me@chrisfromnowhere.me>
2025-05-15 12:37:58 +0800

    feat(web-client): remove loading component border and title
    Signed-off-by: Chris1320 <me@chrisfromnowhere.me>

--- WebClient/src/components/LoadingComponent.tsx
+++ WebClient/src/components/LoadingComponent.tsx
@@ -20,12 +20,14 @@ export const LoadingComponent: React.FC<LoadingComponentProps> = ({
     return (
         <Container size={420} my={40} style={{ paddingTop: "150px" }}>
             <Center>
-                <Paper withBorder={withBorder} shadow="md" p={30} mt={30} radius="md">
+                <Paper withBorder={withBorder} radius="md">
                     <Stack align="center" justify="center" gap="xs">
                         <Image src="/assets/BENTOLogo.svg" alt="BENTO Logo" width={100} height={100} />
+                        {/*
                         <Title className={classes.title} data-testid="loading-title">
                             {Program.name}
                         </Title>
+                        */}
                         {message && <Text data-testid="loading-message">{message}</Text>}
                         <Loader color="blue" type="bars" />
                     </Stack>
