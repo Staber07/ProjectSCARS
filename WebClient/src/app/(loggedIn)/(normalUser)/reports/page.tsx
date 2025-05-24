16c931d2a6362ac8b5dcdfe19bea6a3c2d0c0bb6
rruonan <ramosarabelar@gmail.com>
2025-05-24 15:36:30 +0800

    feat: add button to navigate to liquidation report page

--- WebClient/src/app/(loggedIn)/(normalUser)/reports/page.tsx
+++ WebClient/src/app/(loggedIn)/(normalUser)/reports/page.tsx
@@ -1,8 +1,20 @@
 "use client";
 
-import { Text } from "@mantine/core";
+import { Text, Button } from "@mantine/core";
+import { useRouter } from "next/navigation";
 
 export default function ReportsPage() {
+  const router = useRouter();
+
+  const createReport = () => {
+    router.push("/reports/liquidation-report");
+  };
+
   console.debug("Rendering ReportsPage");
-  return <Text>Reports</Text>;
+  return (
+    <>
+      <Text>Reports</Text>
+      <Button onClick={createReport}>NEW</Button>
+    </>
+  );
 }
