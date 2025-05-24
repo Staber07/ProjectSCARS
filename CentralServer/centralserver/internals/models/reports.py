e8b332a6cc197512db77f73cf39e92f1cb396033
Kirito090504 <tenchieadolfo05@gmail.com>
2025-05-25 00:20:30 +0800

    feat: add School Operation Fund models and update relationships in liquidation report

--- CentralServer/centralserver/internals/models/reports.py
+++ CentralServer/centralserver/internals/models/reports.py
@@ -224,22 +224,63 @@ class LiquidationReportClinicFund(SQLModel, table=True):
     entries: list[AdministrativeExpenseEntry] = Relationship(
         back_populates="parent_report")
     parent_report: "MonthlyReport" = Relationship(
-        back_populates="AdministrativeExpensesReport")
+        back_populates="ClinicFundReport")
 
 
+class SchoolOperationFundCertifiedBy(SQLModel, table=True):
 
+    __tablename__: str = "SchoolOperationFundCertifiedBy" 
 
+    parent: datetime.date = Field(
+        primary_key=True,
+        index=True,
+        foreign_key="ClinicFundCertifiedBy.parent",
+    )
+    user: str = Field(foreign_key="users.id")
 
+    parent_report: "LiquidationReportSchoolOperationFund" = Relationship(
+        back_populates="certified_by"
+    )
 
+class SchoolOperationFundEntry(SQLModel, table=True):
+
+    __tablename__: str = "SchoolOperationFundEntry"  
+
+    parent: datetime.date = Field(
+        primary_key=True,
+        index=True,
+        foreign_key="LiquidationReportSchoolOperationFund.parent",
+    )
+    date: datetime.datetime
+    receipt: str
+    particulars: str
+    unit: str 
+    quantity: float  
+    unit_price: float
+
+    parent_report: "LiquidationReportSchoolOperationFund" = Relationship(
+        back_populates="entries"
+    )
 
 class LiquidationReportSchoolOperationFund(SQLModel, table=True):
     """A model representing the liquidation (School Operation Fund) reports."""
 
-    __tablename__: str = "LiquidationReportSchoolOperationFund"  # type: ignore
+    __tablename__: str = "LiquidationReportSchoolOperationFund" 
 
-    parent: datetime.date = Field(  # TODO: WIP
+    parent: datetime.date = Field(  
         primary_key=True, index=True, foreign_key="monthlyReports.id"
     )
+    certified_by: list[SchoolOperationFundCertifiedBy] = Relationship(
+        back_populates="parent_report"
+    )
+    notedby: str = Field(foreign_key="users.id")
+    preparedby: str = Field(foreign_key="users.id")
+    teacherInCharge: str = Field(foreign_key="users.id")
+    
+    entries: list[SchoolOperationFundEntry] = Relationship(
+        back_populates="parent_report")
+    parent_report: "MonthlyReport" = Relationship(
+        back_populates="SchoolOperationFundReport")
 
 
 class LiquidationReportFacultyAndStudentDevFund(SQLModel, table=True):
