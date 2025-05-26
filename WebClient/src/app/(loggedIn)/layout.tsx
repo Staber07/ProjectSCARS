6b57343d526c9a6cc327a3d0b8f05fa2282ae5ba
Chris1320 <me@chrisfromnowhere.me>
2025-05-27 00:33:04 +0800

    refactor(web-client): clean up layout component by removing onboarding logic
    Signed-off-by: Chris1320 <me@chrisfromnowhere.me>

--- WebClient/src/app/(loggedIn)/layout.tsx
+++ WebClient/src/app/(loggedIn)/layout.tsx
@@ -1,17 +1,18 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-import { useState, useEffect } from "react";
+import { Navbar } from "@/components/Navbar";
+import { CentralServerGetUserInfo } from "@/lib/api/auth";
+import { AuthProvider, useAuth } from "@/lib/providers/auth";
 import { AppShell, ScrollArea } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
-import { OnboardingTour, OnboardingTourStep } from "@gfazioli/mantine-onboarding-tour";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
-import { AuthProvider, useAuth } from "@/lib/providers/auth";
-import { Navbar } from "@/components/Navbar";
-import { CentralServerGetUserInfo, CentralServerUpdateUserInfo } from "@/lib/api/auth";
-import { Program } from "@/lib/info";
-import { notifications } from "@mantine/notifications";
-
+/**
+ * Layout component for logged-in users.
+ * @param {Object} props - The component props.
+ * @param {React.ReactNode} props.children - The child components to render within the layout.
+ */
 export default function LoggedInLayout({ children }: { children: React.ReactNode }) {
     console.debug("Rendering LoggedInLayout");
     return (
@@ -21,111 +22,46 @@ export default function LoggedInLayout({ children }: { children: React.ReactNode
     );
 }
 
+/**
+ * LoggedInContent component that wraps the main content for logged-in users.
+ * @param {Object} props - The component props.
+ * @param {React.ReactNode} props.children - The child components to render within the content area.
+ */
 function LoggedInContent({ children }: { children: React.ReactNode }) {
     const [userRole, setUserRole] = useState<number | null>(null);
     const { isAuthenticated } = useAuth();
-    const router = useRouter();
     const [opened] = useDisclosure();
-    const [onboardingStarted, { open: startOnboarding, close: stopOnboarding }] = useDisclosure(false, {
-        onOpen: () => {
-            console.debug("Onboarding tour started");
-        },
-        onClose: () => {
-            console.debug("Onboarding tour ended");
-        },
-    });
-
-    const updateOnboardingStatus = async () => {
-        console.debug("Updating onboarding status");
-        stopOnboarding();
-        const userInfo = await CentralServerGetUserInfo();
-        const finishedTutorials: string[] = userInfo.finishedTutorials.split(",");
-        finishedTutorials.push("dbob");
-        const updatedUserInfo = {
-            ...userInfo,
-            finishedTutorials: finishedTutorials.join(","),
-        };
-        try {
-            console.debug("Updating user info in server...");
-            await CentralServerUpdateUserInfo(updatedUserInfo);
-            console.debug("User info updated successfully");
-        } catch (error) {
-            console.error("Error updating user info", error);
-            notifications.show({ title: "Error", message: "Error updating user info" });
-        }
-    };
-
-    const onboardingSteps: OnboardingTourStep[] = [
-        {
-            id: "onboarding-navbar-header",
-            title: `Welcome to ${Program.name}`,
-            content: "Let's get you started with a quick tour.",
-        },
-        {
-            id: "onboarding-navbar-dashboard",
-            title: "Dashboard",
-            content: "This is the dashboard. Here you can see an overview of your school's statistics and reports.",
-        },
-        {
-            id: "onboarding-navbar-statistics",
-            title: "Statistics",
-            content: "Here you can see the statistics of your school in graph and table forms.",
-        },
-        {
-            id: "onboarding-navbar-reports",
-            title: "Reports",
-            content: "In this section, you can view and manage financial reports of your school.",
-        },
-        {
-            id: "onboarding-navbar-profile",
-            title: "Profile",
-            content: "You can view and edit your account profile information here.",
-        },
-    ];
+    const router = useRouter();
 
     useEffect(() => {
+        console.debug("LoggedInContent useEffect started", { isAuthenticated });
         const fetchUserInfo = async () => {
             console.debug("Fetching user information");
             const userInfo = await CentralServerGetUserInfo();
             setUserRole(userInfo.roleId);
-            const finishedTutorials: string[] = userInfo.finishedTutorials.split(",");
-            if (!finishedTutorials.includes("dbob")) {
-                console.debug("Starting onboarding tour");
-                startOnboarding();
-            }
         };
-        console.debug("LoggedInContent useEffect started", { isAuthenticated });
         if (!isAuthenticated) {
             router.push("/");
         }
         fetchUserInfo();
-    }, [isAuthenticated, router, startOnboarding]);
+    }, [isAuthenticated, router]);
 
     console.debug("Rendering LoggedInContent", { isAuthenticated });
     return (
-        // FIXME: Onboarding process does not work
-        <OnboardingTour
-            tour={onboardingSteps}
-            started={onboardingStarted}
-            onOnboardingTourEnd={updateOnboardingStatus}
-            onOnboardingTourClose={updateOnboardingStatus}
+        <AppShell
+            navbar={{
+                width: 325,
+                breakpoint: "sm",
+                collapsed: { mobile: !opened },
+            }}
+            padding="md"
         >
-            <AppShell
-                // header={{ height: 60 }}
-                navbar={{
-                    width: 325,
-                    breakpoint: "sm",
-                    collapsed: { mobile: !opened },
-                }}
-                padding="md"
-            >
-                <AppShell.Navbar p="md">
-                    <ScrollArea scrollbars="y">
-                        <Navbar enableAdminButtons={userRole === 1 || userRole === 2} />
-                    </ScrollArea>
-                </AppShell.Navbar>
-                <AppShell.Main>{children}</AppShell.Main>
-            </AppShell>
-        </OnboardingTour>
+            <AppShell.Navbar p="md">
+                <ScrollArea scrollbars="y">
+                    <Navbar enableAdminButtons={userRole === 1 || userRole === 2} />
+                </ScrollArea>
+            </AppShell.Navbar>
+            <AppShell.Main>{children}</AppShell.Main>
+        </AppShell>
     );
 }
