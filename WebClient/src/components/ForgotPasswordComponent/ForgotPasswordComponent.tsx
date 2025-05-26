6dc347d372a2aacdd5261c385c3aa2267d0164d8
Chris1320 <me@chrisfromnowhere.me>
2025-05-26 23:27:56 +0800

    feat(web-client): enhance button with animation effects
    Signed-off-by: Chris1320 <me@chrisfromnowhere.me>

--- WebClient/src/components/ForgotPasswordComponent/ForgotPasswordComponent.tsx
+++ WebClient/src/components/ForgotPasswordComponent/ForgotPasswordComponent.tsx
@@ -170,6 +170,10 @@ export function ForgotPasswordComponent(): React.ReactElement {
                             onClick={() => {
                                 router.push("/login");
                             }}
+                            component={motion.button}
+                            transition={{ type: "spring", stiffness: 500, damping: 30, mass: 1 }}
+                            whileHover={{ scale: 1.05 }}
+                            whileTap={{ scale: 0.95 }}
                         >
                             Back to Login
                         </Button>
