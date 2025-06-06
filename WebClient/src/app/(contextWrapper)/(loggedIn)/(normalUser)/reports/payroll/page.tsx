c4bb2acb6a73ff9962e5edecea95ce77f05b8c36
rruonan <ramosarabelar@gmail.com>
2025-06-06 14:58:29 +0800

    feat: add report pages for Sales, Payroll, and Disbursement Voucher

---
+++ WebClient/src/app/(contextWrapper)/(loggedIn)/(normalUser)/reports/payroll/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import { Text } from "@mantine/core";
+
+export default function PayrollPage() {
+  console.debug("Rendering PayrollPage");
+  return <Text>Payroll</Text>;
+}
---
+++ WebClient/src/app/(contextWrapper)/(loggedIn)/(normalUser)/reports/sales/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import { Text } from "@mantine/core";
+
+export default function SalesPage() {
+  console.debug("Rendering SalesPage");
+  return <Text>Daily/Monthly Sales</Text>;
+}
